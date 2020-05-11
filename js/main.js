09e5084ab96454a11d7c671cfd234a1521f39286
Robert Perillo <perillo.robert@gmail.com>
2020-05-11 09:09:01 -0600

    complete win check logic

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ const slotImage = [
     '<i class="fas fa-frog" style="color: rgb(251, 60, 254)" ></i>',
     '<i class="fas fa-fish" style="color: rgb(255, 140, 0)"></i>',
     '<i class="fas fa-otter" style="color:rgb(141, 174, 100)"></i>',
-    '<i class="fab fa-mailchimp" style="color:rgb(47, 106, 255)"></i>',
-    '<i class="fas fa-skull-crossbones" style="color:rgb(0, 0, 0)"></i>',
-    '<i class="fas fa-dollar-sign" style="color:rgb(34, 171, 27)"></i>'  
+    '<i class="fab fa-mailchimp" style="color:rgb(47, 106, 255)"></i>', 
 ]
 
-const wildWin = '<i class="fas fa-dollar-sign" style="color:rgb(34, 171, 27)"></i>'
-const wildLose = '<i class="fas fa-skull-crossbones" style="color:rgb(0, 0, 0)"></i>'
+const wilds = [
+    '<i class="fas fa-dollar-sign" style="color:rgb(34, 171, 27)"></i>',
+    '<i class="fas fa-skull-crossbones" style="color:rgb(0, 0, 0)"></i>'
+]
 
 /*----- app's state (variables) -----*/
 let bet;
@@ -21,9 +21,11 @@ let slots;
 /*----- cached element references -----*/
 let spinBtn = document.querySelector('#spin-btn button')
 let coinSpan = document.querySelector('#coins div > span')
+let betInput = document.querySelector('#input-bet')
 let slotLeft = document.querySelector('#left')
 let slotCenter = document.querySelector('#center')
 let slotRight = document.querySelector('#right')
+let displayMsg = document.querySelector('#display-msg')
 
 
 /*----- event listeners -----*/
@@ -35,59 +37,107 @@ init()
 function init() {
     bet = 25;
     coins = 500;
-    render()
+    renderImages()
+    renderMoney()
 }
 
-function render(){
+function renderMoney(){
     coinSpan.textContent = coins;
-    slotLeft.innerHTML = slotImage[randNum()];
-    slotCenter.innerHTML = slotImage[randNum()];
-    slotRight.innerHTML = slotImage[randNum()];
+    betInput.value = bet;
+}
+
+function renderImages() {
+    slotLeft.innerHTML = slotImage[randIdx()];
+    slotCenter.innerHTML = slotImage[randIdx()];
+    slotRight.innerHTML = slotImage[randIdx()];
     slots = [slotLeft.innerHTML, slotCenter.innerHTML, slotRight.innerHTML]
 }
 
 
 function spin() {
-    render()
+    bet = parseInt(betInput.value)
+    renderImages()
+    addWildsToArray()
     checkMatch()
-
+    renderMoney()
+    if (slotImage.length > 6) {
+        removeWildFromArray()
+    }
 }
 
 function checkMatch() {
-    checkForWild()
-    threeInRow()
+    if (!checkForWild()) {
+        matchCount()
+    }
 }
 
-function threeInRow() {
-    for (let i = 0; i < slots.length; i++) {
-        if (slots[0] === slots[1]) {
-            if (slots[1] === slots[2]) {
-                console.log('win bucks')
-            }
-        }
+function matchCount() {
+    if (slots[0] === slots[1] && slots[1] === slots[2]) {
+        displayMsg.textContent = `YOU WON ${bet*4}`
+        coins+=bet*4;
     }
+    else if (slots[0] === slots[1] || slots[1] === slots[2]) {
+        displayMsg.textContent = `YOU WON ${bet*2}`
+        coins+=bet*1.5;
+    }
+    else {
+        displayMsg.textContent = 'YOU LOSE'
+        coins-=bet;
+    }
+    console.log('bet', bet)
+    console.log('coins', coins)
 }
 
 
 function checkForWild(){
     let count = 0;
     for (let i = 0; i < slots.length; i++) {
-        if (slots[i] === wildWin) {
-            count++
+        if (slots[i] === wilds[0]) {
+            count++;
+        }
+        if (slots[i] === wilds[1]) {
+            count=4;
         }
     }
     if (count === 1) {
-        console.log('win bucks')
+        displayMsg.textContent = `YOU WON ${bet*2}`
+        coins+=bet*2
+        return true;
     }
     if (count === 2) {
         console.log('win bigger bucks')
+        displayMsg.textContent = `YOU WON ${bet*4}`
+        coins+=bet*4
+        return true;
     }
     if (count === 3) {
-        console.log('win huge bucks')
+        displayMsg.textContent = `YOU WON ${bet*6}`
+        coins+=bet*6
+        return true;
     }
+    if (count === 4) {
+        displayMsg.textContent = 'YOU LOSE'
+        coins-=bet
+        return true;
+    }
+    return false;
 }
 
 
-function randNum() {
+function randIdx() {
     return Math.floor(Math.random() * slotImage.length);
+}
+
+function addWildsToArray() {
+    let randWild = Math.floor(Math.random() * 100)
+    if (randWild >= 80 && randWild <= 90) {
+        slotImage.push(wilds[0])
+    }
+    else if (randWild >= 91 && randWild <= 100) {
+        slotImage.push(wilds[1])
+    }
+}
+
+function removeWildFromArray(){
+    slotImage.pop()
 }
